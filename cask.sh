dcbb6d436edc911f9f4ed7a4aa392820c1a811d9
Ashur Cabrera <hello@cabreramade.co>
2015-12-03 09:41:05 -0800

    added Atom to cask roundup

--- cask.sh
+++ cask.sh
@@ -1,7 +1,6 @@
 # --
 # Install apps using Cask, symlink to /Applications
 # --
+brew cask install --appdir=/Applications atom
 brew cask install --appdir=/Applications bartender
 brew cask install --appdir=/Applications moom
-brew cask install --appdir=/Applications coda
-brew cask install --appdir=/Applications sublime-text
