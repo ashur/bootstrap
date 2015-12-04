bd65cbe4dccc5ae34a1b3026f3136600c5780589
Ashur Cabrera <ashur@panic.com>
2015-12-04 11:26:03 -0800

    updated Git hash alias

--- git.sh
+++ git.sh
@@ -5,7 +5,7 @@ git config --global user.name 'Ashur Cabrera'
 
 # Aliases
 git config --global alias.hist 'log --pretty=oneline'
-git config --global alias.donde 'log --pretty=format:"%H" -n 1'
+git config --global alias.hash 'log --pretty=format:"%h" -n 1'
 
 # Misc
 git config --global status.submoduleSummary true
