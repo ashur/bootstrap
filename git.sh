c4d72978c82f5162155bd7b5346e986cd0a53489
Ashur Cabrera <hello@cabreramade.co>
2017-11-16 07:24:59 -0800

    🔧 Re-organizing Git

--- git.sh
+++ git.sh
@@ -10,13 +10,13 @@ echo "✓ user.name"
 git config --global push.recurseSubmodules check
 echo "✓ push.recurseSubmodules"
 
+git config --global status.submoduleSummary true
+echo "✓ status.submoduleSummary"
+
 # Aliases
 git config --global alias.hist 'log --pretty=oneline'
 echo "✓ alias.hist"
 git config --global alias.hash 'log --pretty=format:"%h" -n 1'
 echo "✓ alias.hash"
 
-# Misc
-git config --global status.submoduleSummary true
-echo "✓ status.submoduleSummary"
 echo
