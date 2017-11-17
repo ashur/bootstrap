945dad0403a5a804bb797e9360c3c7b34be39ba7
Ashur Cabrera <hello@cabreramade.co>
2017-11-17 13:00:47 -0800

    🐛 Adding defaults cache buster

--- moom.sh
+++ moom.sh
@@ -10,6 +10,7 @@ header "Moom"
 # Preferences
 # -----------
 cp ./assets/moom/com.manytricks.Moom.plist "$HOME/Library/Preferences"
+defaults read moom/com.manytricks.Moom.plist &>/dev/null
 task_done "Preferences" "already copied"
 
 echo
