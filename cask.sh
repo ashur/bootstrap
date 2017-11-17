0d69b51d214b8549d7b9f2a663dde09a96cdd211
Ashur Cabrera <hello@cabreramade.co>
2017-11-17 13:39:49 -0800

    🐛 Improving Cask accuracy

--- cask.sh
+++ cask.sh
@@ -13,11 +13,14 @@ function install_cask()
 {
 	APP_FORMULA="$1"
 	APP_NAME="$2"
+	APP_BUNDLE="${APP_NAME}.app"
 
-	brew cask list $APP_FORMULA &>/dev/null
-	if [ $? -ne 0 ]; then
-		task_doing "${APP_NAME}" "Installing"
-		brew cask install --appdir=/Applications "${APP_FORMULA}"
+	if [ ! -d "/Applications/${APP_BUNDLE}" ]; then
+		brew cask list $APP_FORMULA &>/dev/null
+		if [ $? -ne 0 ]; then
+			task_doing "${APP_NAME}" "Installing"
+			brew cask install --appdir=/Applications "${APP_FORMULA}"
+		fi
 	else
 		task_done "${APP_NAME}" "already installed"
 	fi
@@ -25,10 +28,13 @@ function install_cask()
 
 # Casks
 # -----
-install_cask github "Github Desktop"
-install_cask alfred "Alfred"
-install_cask dropbox "Dropbox"
-install_cask 1password "1Password"
-install_cask slack "Slack"
+
+brew_tap_keg "caskroom/versions"
+
+install_cask 1password	"1Password 6"
+install_cask alfred		"Alfred 3"
+install_cask dropbox	"Dropbox"
+install_cask github		"Github Desktop"
+install_cask slack		"Slack"
 
 echo
