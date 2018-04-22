1bace92df7dd32e71c3d7019307e5b22a3e2e8b5
Ashur Cabrera <hello@cabreramade.co>
2018-04-22 11:24:51 -0700

    Add ~/Screenshots

--- macos.sh
+++ macos.sh
@@ -40,6 +40,7 @@ task_done "Screenshot Drop Shadows" "disabled"
 # -----------
 make_directory "${HOME}/Bots"
 make_directory "${HOME}/Developer"
+make_directory "${HOME}/Screenshots"
 make_directory "${HOME}/Sites"
 
 echo
